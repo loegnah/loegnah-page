7c981613bf764f67c2192d48228215616dfa9f9d
loegnah <loegnah@gmail.com>
2022-12-02 23:11:49 +0900

    Applying Tailwind CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
-import './globals.css'
+import "../styles/globals.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body className={"bg-gray-900"}>{children}</body>
     </html>
-  )
+  );
 }
