f16394e168fc404f796ab1138cbba7ece10b2846
loegnah <loegnah@gmail.com>
2022-12-08 21:40:40 +0900

    Remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,7 @@
-'use client';
-
 import tw from 'tailwind-styled-components';
-import { useState } from 'react';
 
 export default function Home() {
-  const [a, setA] = useState(true);
-
-  return (
-    <HomeRoot>
-      <Container $aa={a}>hiahi</Container>
-      <div>hihi</div>
-    </HomeRoot>
-  );
+  return <HomeRoot></HomeRoot>;
 }
 const HomeRoot = tw.main`
-  bg-red-500
-`;
-
-const Container = tw.div<{ $aa: boolean }>`
-  flex bg-slate-500
-  ${({ $aa }) => ($aa ? 'text-red-500' : 'text-yellow-500')}
-  ${({ $aa }) => ($aa ? 'p-5' : null)}
 `;
