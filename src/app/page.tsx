0d34c3fdded25db3d8c2da2b867da1409d43efc6
loegnah <loegnah@gmail.com>
2022-12-08 23:20:40 +0900

    Header position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import tw from 'tailwind-styled-components';
+import MainHeader from '../components/mainHeader';
 
 export default function Home() {
-  return <HomeRoot></HomeRoot>;
+  return (
+    <HomeRoot>
+      <MainHeader />
+    </HomeRoot>
+  );
 }
 const HomeRoot = tw.main`
+  w-[100vw] h-[100vh] bg-deep-1 flex flex-col px-20
 `;
